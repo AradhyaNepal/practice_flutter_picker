510ddc5f8807745cb9c2d9cccf2041824adeab26
Aradhya Nepal <aradhya.1221@gmail.com>
2023-08-01 15:19:41 +0545

    Completed birthday calculator

--- lib/pick_date_screen.dart
+++ lib/pick_date_screen.dart
@@ -1,3 +1,4 @@
+import 'package:age_calculator/age_calculator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_picker/picker.dart';
 import 'package:practice_flutter_picker/widgets/custom_selection.dart';
@@ -10,8 +11,17 @@ class PickDateScreen extends StatefulWidget {
 }
 
 class _PickDateScreenState extends State<PickDateScreen> {
-  late final pickerWidget=Picker(
+  int minimumAgeLimit=18;
+  late final selectedValueController = <int>[];
+  final todayDate=DateTime.now();
+  int startYear=1900;
+  late int endYear=todayDate.year;
+
+  ///This picker is stored in separate variable because [makePicker] method is used
+  ///So if i set this method in build then this widget will rebuild on every setState
+  late final pickerWidget = Picker(
       height: MediaQuery.of(context).size.height * 0.33,
+      selecteds: selectedValueController,
       itemExtent: 40,
       backgroundColor: Colors.grey.shade100,
       textStyle: const TextStyle(
@@ -22,11 +32,36 @@ class _PickDateScreenState extends State<PickDateScreen> {
       selectionOverlay: const CustomSelector(),
       hideHeader: true,
       adapter: DateTimePickerAdapter(
-        customColumnType: [1, 0, 2],
+        value: todayDate.subtract(Duration(days: minimumAgeLimit*365)),
+        yearBegin: startYear,
+        yearEnd: endYear,
+        customColumnType: [1, 2, 0],
       ),
       onSelect: (picker, index, value) {
-        print("ON CHanged $value");
+        setState(() {});
       }).makePicker();
+
+  DateTime get selectedBirthDay =>
+      DateTime(selectedYear, selectedMonth,selectedDay);
+
+  int get selectedYear=>selectedValueController[2] + startYear;
+  int get selectedMonth=>selectedValueController[0] + 1;
+  int get selectedDay=>selectedValueController[1]+1;
+
+  String get ageResult{
+    return AgeCalculator.age(selectedBirthDay).toString();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration.zero,(){
+      setState(() {
+        //Set State is done to sync default selected value with our [selectedValueController]
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -52,12 +87,15 @@ class _PickDateScreenState extends State<PickDateScreen> {
                   ),
                 ),
                 pickerWidget,
-                const SizedBox(height: 20,),
-                const Text(
-                  "Your age",
-                  style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.w500,
+                const SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  "Your age is\n$ageResult",
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
                   ),
                 ),
               ],
